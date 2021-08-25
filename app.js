bd47579ae4bc7eb95e4603826e198f9152463619
Tuncay Üner <tuncayuner@gmail.com>
2021-08-25 17:05:52 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://0.0.0.0:${port}`)
 })
